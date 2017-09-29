6c838a86f2de093ed3ae691f8210de909f4a03e0
AlenaSkurat <skuraten@gmail.com>
2017-09-29 22:11:54 +0300

    The task has been resolved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var list = [],
+      state = [],
+      flag = true;
+  for (var i = 0; i<bracketsConfig.length; i++) {
+    list[i]= bracketsConfig[i].join('');
+  }
+  while (flag) {
+      for(var j = 0; j < list.length; j++) {
+          if(str.indexOf(list[j]) == -1) {
+              state.push(0);       
+          } else {
+              str = str.replace(list[j],'');                
+              state.push(1);   
+          } 
+      }    
+      if (state.indexOf(1) == -1) {
+          flag = false;
+      }      
+      state = [];
+  }
+  return str.length == 0 ? true : false;
 }
